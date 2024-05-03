fb5ef497daa7444d655d4a7b806be564faf7c18c
Agung Purnomo <122212227+Agungpurr@users.noreply.github.com>
2024-05-04 03:14:34 +0700

    index.js

--- index.js
+++ index.js
@@ -23,4 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("notifikasiSisaKarakter").style.color = "black";
     }
   });
+
+  document.getElementById("inputNama").addEventListener("focus",function () {
+    console.log("inputNama: focus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility = "visible";
+  });
+
+   document.getElementById('inputNama').addEventListener('blur', function () {
+      console.log('inputNama: blur');
+      document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden';
+    });
 });
