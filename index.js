c0aa296b25840e96a31388180e28727241704004
Agung Purnomo <122212227+Agungpurr@users.noreply.github.com>
2024-05-04 12:09:41 +0700

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const inputMaxLengthOnLoad = this.documentElementById("inputNama").maxLength;
-  documentElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
+  const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
+  document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
 
   document.getElementById("inputNama").addEventListener("input", function () {
     const jumlahKarakterDiketik =
-      document.getElementById("inputNama").ariaValueMax.length;
+      document.getElementById("inputNama").value.length;
     const jumlahKarakterMaksimal =
       document.getElementById("inputNama").maxLength;
 
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("sisaKarakter").innerText =
       sisaKarakterUpdate.toString();
 
-    if (sisaKarakterUpdate == 0) {
-      document.getElementById("sisaKarakter").innertext =
+    if (sisaKarakterUpdate === 0) {
+      document.getElementById("sisaKarakter").innerText =
         "Batas maksimal tercapai!";
     } else if (sisaKarakterUpdate <= 5) {
       document.getElementById("notifikasiSisaKarakter").style.color = "red";
@@ -24,13 +24,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  document.getElementById("inputNama").addEventListener("focus",function () {
+  document.getElementById("inputNama").addEventListener("focus", function () {
     console.log("inputNama: focus");
-    document.getElementById("notifikasiSisaKarakter").style.visibility = "visible";
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
   });
 
-   document.getElementById('inputNama').addEventListener('blur', function () {
-      console.log('inputNama: blur');
-      document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden';
-    });
+  document.getElementById(i"nputNama").addEventListener("blur", function () {
+    console.log("inputNama: blur");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "hidden";
+  });
 });
